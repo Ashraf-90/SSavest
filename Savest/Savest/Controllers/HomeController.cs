5b4e46c3847bd56929af1f260ea0f907523c40e3
Ashraf-90 <ashraf.90.issa@gmail.com>
2025-11-19 12:39:28 +0300

    test

--- Savest/Savest/Controllers/HomeController.cs
+++ Savest/Savest/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Savest.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //Test
         }
 
         public IActionResult Index()
