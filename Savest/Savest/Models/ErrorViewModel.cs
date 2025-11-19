3e8e6363891783d737b05ba906dc287635da7b5a
Ashraf-90 <ashraf.90.issa@gmail.com>
2025-11-19 12:37:08 +0300

    Create Project

---
+++ Savest/Savest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Savest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
